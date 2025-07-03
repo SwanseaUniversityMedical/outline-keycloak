c68355d7750efafadb1610c0dd121683ae331c5d
joss <joss@chi.swan.ac.uk>
2025-07-03 14:53:21 +0100

    ci: add ci to build container

---
+++ .github/commitlint.config.mjs
@@ -0,0 +1,13 @@
+import { RuleConfigSeverity } from '@commitlint/types';
+
+export default {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
+    'scope-enum': [RuleConfigSeverity.Error, 'always', [
+        '',
+        'deps'
+    ]],
+    'subject-case': [RuleConfigSeverity.Error, 'never', []],
+  }
+};
